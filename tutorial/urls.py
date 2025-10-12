4e386b48943fbcd52347fbd857a9a3ac209b6a35
Mayar <maimagdy238@gmail.com>
2025-10-12 03:02:37 +0300

    add new routers

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -7,15 +7,15 @@ from rest_framework_simplejwt.views import (
 from tutorial.quickstart import views
 
 
+
 router = routers.DefaultRouter()
 router.register(r"users", views.UserViewSet)
-# router.register(r"groups", views.GroupViapewSet),
-router.register(r'UsersApi' , views.UserViewSet_one , basename='userapi' ),
-router.register(r'groupsApi', views.GroupViewSet_One) 
+router.register(r"groups", views.GroupViewSet),
+router.register(r'UsersApi' , views.UserViewSet_one , basename='UsersApi'),
+router.register(r'groupsApi', views.GroupViewSet_One ,  basename='GroupsApi') 
 
 urlpatterns = [
-    path("", include(router.urls)),
-    # authentication for the browsable API
+    path("", include(router.urls)), # authentication for the browsable API
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
